6e3cfddcd4a668ae16df23a1e7ed928ae75e16da
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-09 22:32:43 -0500

    Doc: Created documentation for the hashString() function

--- src/main/java/SecurityUtils.java
+++ src/main/java/SecurityUtils.java
@@ -6,6 +6,16 @@ import java.security.NoSuchAlgorithmException;
 
 public class SecurityUtils {
 	
+	
+	/**
+     * Hashes a given input string using the specified algorithm.
+     * 
+     * @param input The string to be hashed.
+     * @param algorithm The hashing algorithm (MD5, SHA1, SHA256).
+     * @return The hashed string in Base64 format.
+     * @throws NoSuchAlgorithmException If the hashing algorithm is not supported.
+     */
+	
 	 public static String hashString(String input, HashAlgorithm algorithm) throws NoSuchAlgorithmException {
 	        
 		    MessageDigest digest = MessageDigest.getInstance(algorithm.name().replace("SHA1", "SHA-1"));
