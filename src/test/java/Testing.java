87ba664f7def44df291d3cb4f4667419138bc839
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-09 22:20:35 -0500

    Test: Created function for hashing a given string given an enumerated hash algorithm

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -2,11 +2,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.security.NoSuchAlgorithmException;
+
 class Testing {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
-}
+	 @Test
+	    void testHashString() throws NoSuchAlgorithmException {
+		 
+	        String input = "testString";
+	        
+	        String hashed = SecurityUtils.hashString(input, HashAlgorithm.SHA256);
+	        
+	        assertNotNull(hashed);
+	        
+	    }//end of testHashString
+	 
+}//end of test class 
